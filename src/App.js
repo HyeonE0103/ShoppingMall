58e8708960196a9b77e055fdfe7e89fcf443f72a
HyeonE <mag30102000@gmail.com>
2023-02-19 23:47:17 +0900

    코드가 길어질땐 import, exprot

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useState } from "react";
+import data from "./data";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div>
       <Navbar bg="light" variant="light">
@@ -17,26 +21,25 @@ function App() {
       <div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + "/img/shoes2.jpg"} width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="/img/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
+          {shoes.map(function (a, i) {
+            return <Item shoes={shoes[i]} i={i} key={i} />;
+          })}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Item(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={process.env.PUBLIC_URL + "/img/shoes" + (props.i + 1) + ".jpg"}
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
