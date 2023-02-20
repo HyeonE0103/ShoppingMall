bb98a1573940925b17060a0cc557ac3d7d1056aa
HyeonE <mag30102000@gmail.com>
2023-02-21 00:37:57 +0900

    리액트 라우터 navigate, nested routes, outlet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { useState } from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate, Outlet } from "react-router-dom";
 
 import "./App.css";
 import data from "./data";
 import Detail from "./routes/detail";
 
 function App() {
+  let navigate = useNavigate();
   let [shoes] = useState(data);
 
   return (
@@ -17,11 +18,17 @@ function App() {
           <Navbar.Brand href="/">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
+            <Nav.Link href="/event">Event</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <Link to="/detail">상세페이지</Link>
 
       <Routes>
         <Route
@@ -40,7 +47,19 @@ function App() {
           }
         />
         <Route path="/detail" element={<Detail />} />
-        <Route path="/about" element={<div>어바웃페이지임</div>} />
+        <Route
+          path="/event"
+          element={
+            <>
+              <h1>오늘의 이벤트</h1>
+              <Outlet></Outlet>
+            </>
+          }
+        >
+          <Route path="one" element={<h3>첫 주문시 양배추즙 서비스</h3>} />
+          <Route path="two" element={<h3>생일기념 쿠폰받기</h3>} />
+        </Route>
+        <Route path="*" element={<div>없는페이지 404</div>} />
       </Routes>
     </div>
   );
